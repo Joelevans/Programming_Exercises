439966ed4d3526a94ee91fb179f0687fb200c33a
evans.ochieng@strathmore.edu <evans.ochieng@strathmore.edu>
2020-12-28 23:54:33 +0300

    Program that tests the accuracy of Floating-Point Types

---
+++ Ch2-Primitive-Types-and-Variables/RealTypeAccuracy.cs
@@ -0,0 +1,16 @@
+using System;
+
+class RealTypeAccuracy{
+	
+	static void Main(){
+		//Declare Variables
+		float floatPI = 3.141592653589793238f;
+		double doublePI = 3.141592653589793238;
+		float floatExample = 0.123456789f;
+		
+		//Print the results on the console
+		Console.WriteLine("FLoat PI is " + floatPI);
+		Console.WriteLine("Double PI is " + doublePI);
+		Console.WriteLine("Float Example is " + floatExample);
+	}
+}
