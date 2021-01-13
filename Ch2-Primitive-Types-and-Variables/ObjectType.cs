f23a872bb30b06b27be7ceb3a16bfdde5f8e9f9e
evans.ochieng@strathmore.edu <evans.ochieng@strathmore.edu>
2021-01-13 10:56:18 +0300

    Object Type

---
+++ Ch2-Primitive-Types-and-Variables/ObjectType.cs
@@ -0,0 +1,13 @@
+using System;
+
+class ObjectType{
+	
+	static void Main(){
+		object container1 = 5;
+		object container2 = "Five";
+		
+		//print
+		Console.WriteLine("The value of container1 is:" + container1);
+		Console.WriteLine("The value of container2 is:" + container2);
+	}
+}
