a7d9dc151ecfd8384cd7e3738a3eca5a372f83dc
evans.ochieng@strathmore.edu <evans.ochieng@strathmore.edu>
2021-02-05 18:46:22 +0300

    Question 1 done

---
+++ Ch2-Primitive-Types-and-Variables/Exercises/Ch2Exercise.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Ch2Exercise
+{
+	static void Main()
+	{
+		//---Qsn 1--
+		//-------------------------------------
+		/*Declare several variables by selecting for each one of them the most appropriate of the types sbyte,
+		byte, short, ushort, int, uint, long and ulong in order to assign them the following values: 52,130; -115;
+		4825932; 97; -10000; 20000; 224; 970,700,000; 112; -44; -1,000,000; 1990; 123456789123456789.
+		*/
+		ushort numberA = 52130;
+		sbyte numberB = -115;
+		int numberC = 4825932;
+		sbyte numberD = 97;
+		short numberE = -10000;
+		short numberF = 20000;
+		byte numberG = 224;
+		int numberH = 970700000;
+		byte numberJ = 112;
+		sbyte numberK = -44;
+		int numberL = -1000000;
+		short numberM = 1990;
+		ulong numberN = 123456789123456789;
+		
+		
+		//-------------------------------------
+		
+	}
+	
+}
