02cc8d8da2af43d6666f338d5b5127e5e94881b5
evans.ochieng@strathmore.edu <evans.ochieng@strathmore.edu>
2021-02-17 14:06:32 +0300

    Qsn 2 and 3 complete

--- Ch2-Primitive-Types-and-Variables/Exercises/Ch2Exercise.cs
+++ Ch2-Primitive-Types-and-Variables/Exercises/Ch2Exercise.cs
@@ -23,10 +23,65 @@ class Ch2Exercise
 		int numberL = -1000000;
 		short numberM = 1990;
 		ulong numberN = 123456789123456789;
+		//-------------------------------------
 		
 		
+		
+		//---Qsn 2--
+		//-------------------------------------
+		/*
+		Which of the following values can be assigned to variables of type float, double and decimal: 5, -5.01,
+		34.567839023, 12.345; 8923.1234857; 3456.091124875956542151256683467?
+		*/
+		float floatA = 5f;
+		float floatB = -5.01f;
+		double doubleA = 34.567839023;
+		float floatC = 12.345f;
+		float floatD = 8923.1234857f;
+		decimal decimalA = 3456.091124875956542151256683467m;
+		
+		Console.WriteLine(floatA);
+		Console.WriteLine(floatB);
+		Console.WriteLine(doubleA);
+		Console.WriteLine(floatC);
+		Console.WriteLine(floatD);
+		Console.WriteLine(decimalA);
+		//-------------------------------------
+		
+		//---Qsn-3--
+		//-------------------------------------
+		/*Write a program, which compares correctly two real numbers with accuracy at least 0.000001.
+		*/
+		/// Two floating point variables are considered equal if their difference is less than some 
+		/// predefined precision (e.g 0.000001):
+		/// bool equal = Math.Abs(a - b) < 0.000001;
+		
+		float a = 100.000001f;
+		float b = 100.000302f;
+		bool equal = Math.Abs(a -b) < 0.000001;
+		if(equal)
+		{
+			Console.WriteLine("The numbers are equal");
+		}
+		else
+		{
+			Console.WriteLine("The numbers are not equal");
+		}
 		//-------------------------------------
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
