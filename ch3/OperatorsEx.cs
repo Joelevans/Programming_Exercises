2830dd01e54d3af4c528a973aac97e465950f4df
Evans Ochola <joelevans14@gmail.com>
2021-11-08 19:13:00 +0300

    completed no. 15

--- ch3/OperatorsEx.cs
+++ ch3/OperatorsEx.cs
@@ -179,5 +179,26 @@ class OperatorsEx
         // }
         // Console.WriteLine(isPrime);
         
+        // /*15*/
+        // int number = int.Parse(Console.ReadLine());
+        // int bit3 = (number >> 3) & 1;
+        // int bit4 = (number >> 4) & 1;
+        // int bit5 = (number >> 5) & 1;
+
+        // int bit24 = (number >> 24) & 1;
+        // int bit25 = (number >> 25) & 1;
+        // int bit26 = (number >> 26) & 1;
+
+        // number = number & (~ (1 << 24)) | (bit3 << 24);
+        // number = number & ( ~(1 << 3)) | (bit24 << 3);
+
+        // number = number & (~ (1 << 25)) | (bit4 << 25);
+        // number = number & ( ~(1 << 4)) | (bit25 << 4);
+
+        // number = number &  ( ~(1 << 5)) | (bit26 << 5) ;
+        // number = number & (~ (1 << 26)) | (bit5 << 26);
+
+        // Console.WriteLine(number);
+        // Console.WriteLine(bit26);
     }
 }
