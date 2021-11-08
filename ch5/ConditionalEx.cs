e6ffee3da4499b56bb40b4267851af3cd6d181e9
Evans Ochola <joelevans14@gmail.com>
2021-11-08 20:55:32 +0300

    completed no. 1

---
+++ ch5/ConditionalEx.cs
@@ -0,0 +1,22 @@
+using System;
+
+class ConditionalEx
+{
+    static void Main(string[] args)
+    {
+        // /*1*/
+        // int a = 20;
+        // int b = 15;
+        // int c = 0;
+
+        // if (a > b)
+        // {
+        //     c = a;
+        //     a = b;
+        //     b = c;
+        // }
+        // Console.WriteLine("a is {0}", a);
+        // Console.WriteLine("b is {0}", b);
+
+    }
+}
