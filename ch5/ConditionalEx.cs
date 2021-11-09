ec60e5f7dd848c8d6ae3be400a65979f4c983a80
Evans Ochola <joelevans14@gmail.com>
2021-11-09 11:48:27 +0300

    completed no 5

--- ch5/ConditionalEx.cs
+++ ch5/ConditionalEx.cs
@@ -130,6 +130,60 @@ class ConditionalEx
         // Console.WriteLine(smallest);
 
         /*5*/
-        
+        // Console.WriteLine("Input digit [0 -9] :");
+        // int digit = int.Parse(Console.ReadLine());
+
+        // while ((digit < 0) || (digit > 9))
+        // {
+        //     Console.WriteLine("Please Input digit [0 -9] :");
+        //     digit = int.Parse(Console.ReadLine());
+        // }
+
+        // switch (digit)
+        // {
+        //     case 0:
+        //     Console.WriteLine("Zero");
+        //     break;
+
+        //     case 1:
+        //     Console.WriteLine("One");
+        //     break;
+
+        //     case 2:
+        //     Console.WriteLine("Two");
+        //     break;
+
+        //     case 3:
+        //     Console.WriteLine("Three");
+        //     break;
+            
+        //     case 4:
+        //     Console.WriteLine("Four");
+        //     break;
+            
+        //     case 5:
+        //     Console.WriteLine("Five");
+        //     break;
+
+        //     case 6:
+        //     Console.WriteLine("Six");
+        //     break;
+
+        //     case 7:
+        //     Console.WriteLine("Seven");
+        //     break;
+
+        //     case 8:
+        //     Console.WriteLine("Eight");
+        //     break;
+
+        //     case 9:
+        //     Console.WriteLine("Nine");
+        //     break;
+
+        //     default:
+        //     Console.Write("out of range");
+        //     break;
+        // }
     }
 }
