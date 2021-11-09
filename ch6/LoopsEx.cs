e787e1e01e89a1aadd93162415cab626272f5533
Evans Ochola <joelevans14@gmail.com>
2021-11-09 14:08:37 +0300

    completed no. 2

--- ch6/LoopsEx.cs
+++ ch6/LoopsEx.cs
@@ -11,5 +11,15 @@ class LoopsEx
         // {
         //     Console.WriteLine(i);
         // }
+
+        /*2*/
+        // int n = int.Parse(Console.ReadLine());
+        // for (int i = 1; i <= n; i++)
+        // {
+        //     if (i % (3 * 7) != 0)
+        //     {
+        //         Console.WriteLine(i);
+        //     }
+        // }
     }
 }
