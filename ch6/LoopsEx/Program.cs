358ec18a81de8f2e4c7c46218d7ff67ba12396e0
Evans Ochieng <evansochola14@gmail.com>
2022-04-22 16:25:23 +0300

    No.1 completed

---
+++ ch6/LoopsEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LoopsEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1*/
+            /*
+            Console.WriteLine("Enter a range");
+            int input = int.Parse(Console.ReadLine());
+            
+            for(int i = 1; i <= input; i++)
+            {
+	      Console.WriteLine(i);
+            }
+            */
+        }
+    }
+}
