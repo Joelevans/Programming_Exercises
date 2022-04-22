db85f886d4f8cc3f42312b5dc1eb7100e00a2105
Evans Ochieng <evansochola14@gmail.com>
2022-04-22 16:34:04 +0300

    No.2 completed

--- ch6/LoopsEx/Program.cs
+++ ch6/LoopsEx/Program.cs
@@ -16,6 +16,27 @@ namespace LoopsEx
 	      Console.WriteLine(i);
             }
             */
+            
+            /*2*/
+            /*
+            Console.WriteLine("Enter a range");
+            int input = int.Parse(Console.ReadLine());
+            
+            for(int i = 1; i <= input; i++)
+            {
+	      bool divisibleBy3 = (i%3==0);
+	      bool divisibleBy7 = (i%7==0);
+	      
+	      if (!divisibleBy3 && !divisibleBy7)
+	      {
+		Console.WriteLine(i);
+	      }
+	      else
+	      {
+		Console.WriteLine("divisible by either 3 or 7");
+	      }
+            }
+            */
         }
     }
 }
