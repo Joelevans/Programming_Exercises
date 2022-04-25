fdc10b2aa616b94db4cda462aab4c3fc3683e691
Evans Ochieng <evansochola14@gmail.com>
2022-04-25 16:56:12 +0300

    Update: do while loop

---
+++ ch6/Factorial/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	  
+        
+	    // Using an infinite loop
+	    /*
+	     
+            Console.WriteLine("Enter a positive integer: ");
+            int input = int.Parse(Console.ReadLine());
+            
+            //128 bit integer maximum value of input is 27!
+            decimal factorial = 1;
+            
+            while(true)
+            {
+	      if(input <= 1)
+	      {
+		break;
+	      }
+	      
+	      factorial *=input;
+	      input--;
+	      
+            }
+            Console.WriteLine("n! =  " + factorial);
+	    */
+	      
+	      
+	    // Using a do while loop
+	    Console.WriteLine("Enter a positive integer: ");
+	    int input = int.Parse(Console.ReadLine());
+	    
+	    //new datatype to store big integers, OverflowException will not occur
+	    BigInteger factorial = 1;
+	    
+	    do
+	    {
+	      factorial *= input;
+	      input--;
+	    } while(input > 0);
+	    
+	    Console.WriteLine("n! = " + factorial);
+	    
+	}
+    }
+}
---
+++ ch6/Factorial/README.md
@@ -0,0 +1,11 @@
+#Factorial
+
+Factorial is the product of all positive integers less than or equal
+to a given positive integer.
+It is denoted by that integer and an exclamation point.
+
+7! meaning 7x6x5x4x3x2x1
+n! can be expressed by a factorial of integers less than n i.e
+
+n! = (n-1)! * n
+0! = 1
