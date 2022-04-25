6f06b6ad5c662818dd73ffe9c51d2f40ed33a3ae
Evans Ochieng <evansochola14@gmail.com>
2022-04-25 10:51:00 +0300

    Check if a number is prime

--- ch6/PrimeNumbers/Program.cs
+++ ch6/PrimeNumbers/Program.cs
@@ -6,7 +6,28 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+	    //Read prime-numbers.txt
+        
+            //Check whether a given number is prime or not
+            
+            Console.Write("Enter a positive number: ");
+            int input = int.Parse(Console.ReadLine());
+            
+            int divider = 2;
+            int maxDivider = (int)Math.Sqrt(input);
+            
+            bool prime = true;
+            
+            while(prime && divider <= maxDivider)
+            {
+	      if(input % divider == 0)
+	      {
+		prime = false;
+	      }
+	      divider++;
+	    }
+	    
+	    Console.WriteLine(input + " is prime?" + prime);
         }
     }
 }
