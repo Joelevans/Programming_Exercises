e4c7ad3c18012ece986db4fa40c3a24e675ae5af
Evans Ochieng <evansochola14@gmail.com>
2022-04-26 16:47:00 +0300

    Printing a Triangle using a for loop

---
+++ ch6/PrintTriangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrintTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int numberOfLines = int.Parse(Console.ReadLine());
+           
+           for(int i = 1; i <= numberOfLines; i++)
+           {
+	      for(int j = 1; j <= i; j++)
+	      {
+		Console.Write(j + " ");
+	      }
+	      
+	      Console.WriteLine();
+           }
+        }
+    }
+}
