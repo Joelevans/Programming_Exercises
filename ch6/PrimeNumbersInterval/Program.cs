a9fe2bc1a0663681957a81b04672fbb9a2543b06
Evans Ochola <joelevans14@gmail.com>
2022-04-26 22:27:24 +0300

    Print all prime numbers in the interval [n..m]

---
+++ ch6/PrimeNumbersInterval/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PrimeNumbersInterval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Print all prime numbers in the interval n..m
+           Console.Write("n: ");
+           int n = int.Parse(Console.ReadLine());
+           
+           Console.Write("m: ");
+           int m = int.Parse(Console.ReadLine());
+
+            for (int i = n; i <= m; i++)
+            {
+                bool isPrime = true;
+                int divider = 2;
+                int maxDivider = (int)Math.Sqrt(i);
+
+                while (isPrime && divider <= maxDivider)
+                {
+                    if(i % divider == 0)
+                    {
+                        isPrime = false;
+                    }
+
+                    divider++;               
+                }
+
+                if (isPrime)
+                {
+                   Console.Write(i + " ");    
+                }
+            }
+        }
+    }
+}
