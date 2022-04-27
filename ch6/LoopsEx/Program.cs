db04ed8d4eebebcad8652448ee0852e374c0763e
Evans Ochieng <evansochola14@gmail.com>
2022-04-27 16:53:24 +0300

    no.4 completed

--- ch6/LoopsEx/Program.cs
+++ ch6/LoopsEx/Program.cs
@@ -37,6 +37,80 @@ namespace LoopsEx
 	      }
             }
             */
+            
+            /*3*/
+            /*
+            Console.WriteLine("Enter a series of positive integers: ");
+            int smallest = 1;
+            int largest = 0;
+            int input = 0;
+            for(int i = 0; i < 5; i++)
+            {
+	      input = int.Parse(Console.ReadLine());
+            
+	      
+	      
+	      if (input > largest)
+	      {
+		largest = input;
+	      }
+	      else
+	      {
+		smallest = input;
+	      }
+	      
+            }
+            
+            Console.WriteLine($"smallest is : {smallest} and largest is : {largest}");
+            */
+            
+            /*4*/
+            for(int suit = 1; suit <= 4; suit++)
+            {
+            
+             switch(suit)
+	      {
+		case 1:
+		Console.Write("club:\t\t");
+		break;
+		
+		case 2:
+		Console.Write("diamond:\t");
+		break;
+		
+		case 3:
+		Console.Write("heart:\t\t");
+		break;
+		
+		case 4:
+		Console.Write("spades:\t\t");
+		break;
+		
+		default:
+		Console.Write("invalid");
+		break;
+	      }
+            
+	      for(int card = 2; card <= 14; card++)
+	      {
+		switch(card)
+		{
+		  case 11:
+		    continue; break;
+		  case 12:
+		    Console.Write("Q"); break;
+		  case 13:
+		    Console.Write("K"); break;
+		  case 14:
+		    Console.Write("A"); break;
+		  default:
+		    Console.Write(card); break;
+		}
+	      }
+	      
+	      Console.WriteLine();
+	      
+            }
         }
     }
 }
