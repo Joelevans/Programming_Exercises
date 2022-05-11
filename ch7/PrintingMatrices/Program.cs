1bcaec781402e9ea9626f18f5fc0343738238673
Evans Ochieng <evansochola14@gmail.com>
2022-05-11 12:59:16 +0300

    Printing Matrices

---
+++ ch7/PrintingMatrices/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrintingMatrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare and initialize a matrix of size 2 x 4
+            
+            int[,] matrix = 
+            {
+	      {1,2,3,4},
+	      {5,6,7,8},
+            };
+            
+            //Print the matrix on the console
+            for(int row = 0; row < matrix.GetLength(0); row++)
+            {
+	      for(int col = 0; col < matrix.GetLength(1); col++)
+	      {
+		Console.Write(matrix[row, col]);
+	      }
+	      Console.WriteLine();
+            }
+        }
+    }
+}
