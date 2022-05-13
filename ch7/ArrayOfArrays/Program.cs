6ce4874b11584cea5d961a853e49b840decbb6fe
Evans Ochieng <evansochola14@gmail.com>
2022-05-13 16:30:34 +0300

    Jagged Arrays

---
+++ ch7/ArrayOfArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ArrayOfArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //jagged arrays 
+            
+            int[][] jaggedArray;
+            jaggedArray = new int[2][];
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[3];
+            
+            int[][] myJaggedArray = 
+            {
+	      new int[] {5, 7, 2},
+	      new int[] {10, 20, 40},
+	      new int[] {3, 25}
+            };
+            
+            Console.WriteLine($"{myJaggedArray[0][2]}");
+            
+            int[][,] jaggedOfMulti = new int[2][,];
+            jaggedOfMulti[0] = new int[,] 
+            {
+	      { 5, 15 },
+	      { 125, 206 }
+            };
+            
+            jaggedOfMulti[1] = new int[,]
+            {
+	      { 3, 4, 5 },
+	      { 7, 8, 9 }
+            };
+        }
+    }
+}
