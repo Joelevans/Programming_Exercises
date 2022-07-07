301d2412ed7d6d0b952ff1e055ffd049320c7b3d
Evans Ochieng <evansochola14@gmail.com>
2022-07-07 09:04:41 +0300

    test exercises

---
+++ test/builderTriangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace builderTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int blocks = int.Parse(Console.ReadLine());
+            int i = 1;
+            int height = 0; 
+            
+            while(i < blocks)
+            {
+	      i++;
+	      blocks -= i;
+	      height = i;
+            }
+            
+            Console.WriteLine("The height of the pyramid is" + height);
+        }
+    }
+}
---
+++ test/ordinal/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ordinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong ordinal,input = 1;
+            
+	  while(input > 0)
+	  {
+	      Console.WriteLine("Enter an integer: ");
+	      
+	      input = ulong.Parse(Console.ReadLine());
+	      ordinal = input % 10;
+	      
+	      
+	      switch(ordinal)
+	      {
+		case 1:
+		    if(input == 11)
+		    {
+		      Console.WriteLine(input + "th");
+		    }
+		    else
+		    {
+			Console.WriteLine(input + "st");
+		    }
+		  break;
+		  
+		case 2:
+		    if(input == 12)
+		    {
+		      Console.WriteLine(input + "th");
+		    }
+		    else
+		    {
+		      Console.WriteLine(input + "nd");
+		    }
+		  break;
+		  
+		case 3:
+		    if(input == 13)
+		    {
+		      Console.WriteLine(input + "th");
+		    }
+		    else
+		    {
+		      Console.WriteLine(input + "rd");
+		    }
+		  break;
+		  
+		default:
+		    Console.WriteLine(input + "th");
+		    break;
+	      }
+         }
+        }
+    }
+}
