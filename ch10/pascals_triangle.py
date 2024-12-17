926a32bec9d46490a718bd4cab54cf2b9d636798
Evans Ochola <evansochola14@gmail.com>
2024-12-17 10:50:12 +0000

    add recursion-problem: pascals_triangle

---
+++ ch10/pascals_triangle.py
@@ -0,0 +1,20 @@
+def pascals_triangle(n):
+    """Recursive function to calculate Pascals Triangle."""
+    if n == 1:
+        return [[1]] # Base case termination condition
+    else:
+        result = pascals_triangle(n-1)
+
+        # Calculate current row
+        current_row = [1]
+        previous_row = result[-1] # Take from end of result
+
+        for i in range(len(previous_row)-1):
+            current_row.append(previous_row[i]+previous_row[i+1])
+        current_row += [1] # row ends with another 1
+        result.append(current_row)
+        return result
+
+triangle = pascals_triangle(5)
+for row in triangle:
+    print(row)
